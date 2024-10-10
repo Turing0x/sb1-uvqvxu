33f74b42d0361fa8e89cc1bd6c5b5b1f40cfbfab
Turing0x <rauligf97@gmail.com>
2024-10-09 21:39:13 -0400

    text hero changed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,14 +9,14 @@ const Hero: React.FC<Props> = ({ language }) => {
         <h2 className="text-4xl sm:text-5xl font-bold mb-6">MARCEL AL PRODUCTIONS</h2>
         <p className="text-lg sm:text-xl mb-6 sm:mb-8 max-w-2xl sm:max-w-3xl mx-auto">
           {language === 'es' 
-            ? 'Somos una empresa especializada en diseño y desarrollo web dedicada a ofrecer soluciones personalizadas para desarrollo de e-commerce, marketplaces innovadores, optimización SEO, gestión de data entry y soporte multilingüe.' 
-            : 'We are a company specialized in web design and development dedicated to offering customized solutions for the development of e-commerce, innovative marketplaces, SEO optimization, data entry management and multilingual support.'
+            ? 'Somos una empresa especializada en diseño gráfico y producción audiovisual, dedicada a personalizar las soluciones de nuestros clientes para mejorar el alcance de sus plataformas de comercio electrónico y marketplaces. Trabajamos en la creación de marcas, optimización SEO y gestión de entrada de datos para el soporte multilingüe y las transcripciones de videos.' 
+            : "We are a company specialized in graphic design and audiovisual production, dedicated to customizing our clients' solutions to enhance the reach of their e-commerce platforms and marketplaces. We work on branding, SEO optimization, and data entry management for multilingual support and video transcriptions."
           }
         </p>
         <p className="text-md sm:text-lg mb-8 sm:mb-10 max-w-xl sm:max-w-2xl mx-auto">
           {language === 'es' 
-            ? 'En Marcel AL Productions, trabajamos en estrecha colaboración con nuestros clientes para garantizar que cada proyecto refleje su visión y maximice su presencia en el mercado global.' 
-            : 'At Marcel AL Productions, we work closely with our clients to ensure that each project reflects their vision and maximizes their presence in the global marketplace.'
+            ? 'En Marcel AL Productions, trabajamos en estrecha colaboración con nuestros clientes para asegurarnos de que cada proyecto refleje su visión y maximice su presencia en el mercado global.' 
+            : 'At Marcel AL Productions, we work closely with our clients to ensure that each project reflects their vision and maximizes their presence in the global marketplace.'
           }
         </p>
         <a href="#contact" className="btn btn-secondary px-6 py-3 sm:px-8 sm:py-4">
